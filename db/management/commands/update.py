92ec3d9b1b74efe2ec8dd538a3babe2bc4744afc
attar90 <a.attar90@gmail.com>
2024-07-22 01:45:29 +0330

    update

--- db/management/commands/update.py
+++ db/management/commands/update.py
@@ -76,10 +76,10 @@ class Command(BaseCommand):
                             b.add_colors_to_product(product_object.id, colors)
 
 
-                        if len(colors) != 0:
-                            variants = b.get_product_variants(product_object.id)
-                            for variant in variants:
-                                b.update_product_variant(product_object.id, variant['id'],
+                        
+                        variants = b.get_product_variants(product_object.id)
+                        for variant in variants:
+                            b.update_product_variant(product_object.id, variant['id'],
                                                          {'product_identifier': biid_product['barcode'],
                                                           "price": biid_product['price'],
                                                           'compare_at_price': biid_product['compare_at_price']})
@@ -93,8 +93,6 @@ class Command(BaseCommand):
                         product_object.commit = True
                         product_object.save()
                         print(f"update succesfuly {product_object.id}")
-
-                    else:
-                        print("Error")
+               
 
 
